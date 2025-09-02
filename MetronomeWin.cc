813e4e34ec9db33daa619b31fae771c667be2705
Archonic <55026152+Archonic944@users.noreply.github.com>
2025-09-01 20:03:23 -0400

    Normalize file path - Windows

--- MetronomeWin.cc
+++ MetronomeWin.cc
@@ -1,15 +1,40 @@
-#include <windows.h>
+#include <Windows.h>
+#include <mmsystem.h>
 #include <string>
+#include <libgen.h>
+#include "Metronome.h"
 
-std::string GetExecutablePath() {
-    char path[MAX_PATH];
-    GetModuleFileNameA(NULL, path, MAX_PATH);
-    std::string executablePath(path);
-    return executablePath.substr(0, executablePath.find_last_of("\\/")) + "\\";
+class MetronomeWinImpl {
+    public:
+        std::string filename;
+};
+
+std::string getExecutableDir() {
+    char exePath[MAX_PATH];
+    GetModuleFileNameA(NULL, exePath, MAX_PATH);
+    std::string exeDir = exePath;
+    size_t pos = exeDir.find_last_of("\\/");
+    if (pos != std::string::npos) {
+        exeDir = exeDir.substr(0, pos);
+    }
+    return exeDir;
 }
 
-void Metronome::init(const std::string& soundFilePath) {
-    std::string executableDir = GetExecutablePath();
-    std::string fullPath = executableDir + soundFilePath;
-    // ... rest of the initialization code
+Metronome::Metronome() {
+    impl = new MetronomeWinImpl();
+}
+
+bool Metronome::init(const std::string& soundFile) {
+    std::string exeDir = getExecutableDir();
+    std::string fullPath = exeDir + "\\" + soundFile;
+    ((MetronomeWinImpl*)impl)->filename = fullPath;
+    return true;
+}
+
+void Metronome::playClick() {
+    PlaySoundA(((MetronomeWinImpl*)impl)->filename.c_str(), NULL, SND_FILENAME | SND_ASYNC);
+}
+
+Metronome::~Metronome() {
+    delete (MetronomeWinImpl*)impl;
 }
